6fd82fd1d0a52efdeb3edc989643151e80cb523f
Mike D. Pascal <96785933+Mikeprogrammer973@users.noreply.github.com>
2025-06-30 13:51:45 -0400

    Update routes.js

--- assets/js/utils/routes.js
+++ assets/js/utils/routes.js
@@ -2,7 +2,7 @@ export const routes = {
    "profile": ["*", "Tic Tac Toe | Profile"],
    "home": ["*", "Tic Tac Toe | Home"],
    "new-pc": ["*", "Tic Tac Toe | vs PC"],
-   "new-local": ["*", "vTic Tac Toe | vs Local"],
+   "new-local": ["*", "Tic Tac Toe | vs Local"],
    "new-online": ["*", "Tic Tac Toe | vs Online"],
    "settings": ["*", "Tic Tac Toe | Settings"]
 }
